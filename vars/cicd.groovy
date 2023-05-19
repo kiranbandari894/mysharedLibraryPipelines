3781106d6e822ad10533d0f303c01552fa52a5b8
kiranbandari894 <94761136+kiranbandari894@users.noreply.github.com>
2023-05-19 22:46:47 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -1,1 +1,21 @@
+// for downloading repositories from github
+def getGit(repo){
+   git ${repo}
+}
+
+// for building .war
+def buildApp(){
+   sh "maven package"
+}
+// for Deploying build to servers
+def DeployApp(workspace,ip,deploytoappname){
+  sh "scp /var/jenkins_home/workspace/${workspace}/webapp/target/webapp.war  ubuntu@${ip}:/usr/local/tomee/webapps/${deploytoappname}.war"
+}
+
+// for testing 
+def Test(repo,workspace,jar_name){
+  git  ${repo}
+  sh 'java -jar /var/jenkins_home/workspace/${workspace}/${jar_name}.jar'
+}
+
 
