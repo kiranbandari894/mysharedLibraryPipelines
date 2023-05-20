93872fa3b3421aec612e557119cd72cca2977edc
kiranbandari894 <94761136+kiranbandari894@users.noreply.github.com>
2023-05-20 10:17:07 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -8,14 +8,18 @@ def buildApp(){
    sh "mvn package"
 }
 // for Deploying build to servers
-def DeployApp(ip,deploytoappname){
-   deploy adapters: [tomcat9(credentialsId: 'd9cb3bc8-87b7-4f0f-9bd5-715451addf26', path: '', url: 'http://${ip}:8080/')], contextPath: 'mywebapp', war: '**/*.war'
+def DeployAppTesting(ip,deploytoappname){
+   deploy adapters: [tomcat9(credentialsId: 'd9cb3bc8-87b7-4f0f-9bd5-715451addf26', path: '', url: 'http://172.19.0.3:8080/')], contextPath: 'mywebapp', war: '**/*.war'
 }
 
 // for testing 
 def Test(repo,workspace,jar_name){
   git  "${repo}"
   sh "java -jar /var/jenkins_home/workspace/${workspace}/${jar_name}.jar"
+}
+
+def DeployAppProd(ip,deploytoappname){
+   deploy adapters: [tomcat9(credentialsId: 'd9cb3bc8-87b7-4f0f-9bd5-715451addf26', path: '', url: 'http://172.19.0.4:8080/')], contextPath: 'mywebapp', war: '**/*.war'
 }
 
  
