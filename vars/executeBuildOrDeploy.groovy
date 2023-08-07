4d8bf62adc0c19f8e640dc049c0eb1260cb11e12
kiranbandari894 <94761136+kiranbandari894@users.noreply.github.com>
2023-08-07 10:11:59 +0530

    Create executeBuildOrDeploy.groovy

---
+++ vars/executeBuildOrDeploy.groovy
@@ -0,0 +1,26 @@
+def call(String command){
+  if(command == "build"){
+     pipeline{
+       agent any
+       stages{
+         stage("Build"){
+            steps{
+              sh 'mvn -v'
+            }
+         }
+       }
+     }
+  }
+  else if(command == "deploy"){
+    pipeline{
+      agent any
+      stages{
+        stage("deploying"){
+          steps{
+            echo "deploying your app"
+          }
+        }
+      }
+    }
+  }
+}
